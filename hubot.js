c8c4fba0552cb2e2234b90b451beb069134f4858
Giles Lavelle <giles.lavelle@gmail.com>
2016-06-20 13:34:06 -0700

    emoji

--- hubot.js
+++ hubot.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const ascii = require('./index');
 
 module.exports = robot => {
-    robot.respond(/ascii me\s(\S+)(\s\S+)?(\s\S+)?/i, response => {
+    robot.respond(/ascii me\s(\S+)(\s\S+)?/i, response => {
         const match = response.match;
 
         if (!match[1]) {
@@ -10,8 +10,8 @@ module.exports = robot => {
             return;
         }
 
-        const fillChar = match[2] ? _.trim(match[2]) : '#';
-        const spaceChar = match[3] ? _.trim(match[3]) : '_';
+        const fillChar = match[2] ? _.trim(match[2]) : ':fire:';
+        const spaceChar = ':spacergif:';
 
         response.send('\n' + ascii(match[1], fillChar, spaceChar));
     });
